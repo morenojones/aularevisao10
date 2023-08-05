b246f8b98baa8d9f120b94adda6d7351f52048e0
morenojones <morenojones2004@gmail.com>
2023-08-05 12:50:03 -0300

    aula10

---
+++ main.py
@@ -0,0 +1,75 @@
+'''
+IMC = Peso ÷ (Altura x Altura)
+condicionais:         imprima:
+MENOR QUE 18,5	    MAGREZA	   
+ENTRE 18,5 E 24,9	NORMAL	    
+ENTRE 25,0 E 29,9	SOBREPESO	    I
+ENTRE 30,0 E 39,9	OBESIDADE	    II
+MAIOR QUE 40,0	    OBESIDADE GRAVE	III
+
+Faça um programa que calcule o imc 
+e imprima o resultado de acordo com a tabela acima.
+obs: float()
+'''
+
+# peso = float(input('Digite seu peso: '))
+# altura = float(input("Digite sua altura: "))
+# imc = peso/altura**2
+
+# if imc < 18.5:
+#     print('Magreza')
+# elif imc >= 18.5 and imc < 24.9:
+#     print('Normal')
+# elif imc >= 25.0 and imc < 29.9:
+#     print('Soprepeso 1')
+# elif imc >= 30.0 and imc < 39.9:
+#     print('Sobrepeso')
+# else:
+#     print('Obesidade grave 3')
+
+
+'''
+Pergute ao nutricionista quantas medidas ele irá analisar,
+e fça que seu programa acima rode essa quantidade de vezes.
+'''
+# vezes = int(input('Quantas medidas você irá analisar: '))
+# for i in range(0, vezes):
+#     peso = float(input('Digite seu peso: '))
+#     altura = float(input("Digite sua altura: "))
+#     imc = peso/altura**2
+
+#     if imc < 18.5:
+#         print('Magreza')
+#     elif imc >= 18.5 and imc < 24.9:
+#         print('Normal')
+#     elif imc >= 25.0 and imc < 29.9:
+#         print('Soprepeso 1')
+#     elif imc >= 30.0 and imc < 39.9:
+#         print('Sobrepeso')
+#     else:
+#         print('Obesidade grave 3')
+
+'''
+Pergunte ao nutricionista se ele deseja continuar a usar o programa ou nã
+# '''
+# continuar = input('Você deseja continuar a usar o programa, digite s, se não, digite sair: ')
+# while continuar == 's':
+    
+#     vezes = int(input('Quantas medidas você irá analisar: '))
+#     for i in range(0, vezes):
+#         peso = float(input('Digite seu peso: '))
+#         altura = float(input("Digite sua altura: "))
+#         imc = peso/altura**2
+
+#         if imc < 18.5:
+#             print('Magreza')
+#         elif imc >= 18.5 and imc < 24.9:
+#             print('Normal')
+#         elif imc >= 25.0 and imc < 29.9:
+#             print('Soprepeso 1')
+#         elif imc >= 30.0 and imc < 39.9:
+#             print('Sobrepeso')
+#         else:
+#             print('Obesidade grave 3')
+
+#     continuar = input('Você deseja continuar a usar o programa, digite s, se não, digite sair: ')
---
+++ while (1).py
@@ -0,0 +1,4 @@
+resultado = input('Digite s par continuar o programa, e sair, para parar o programa. ')
+while resultado == 's':
+    print('...logica acontece...')
+    resultado = input('Digite s par continuar o programa, e sair, para parar o programa. ')
